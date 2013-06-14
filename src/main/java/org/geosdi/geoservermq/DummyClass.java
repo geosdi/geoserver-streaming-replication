4ad23da54f7ee4913cee1aafec1739124b92e879
naz <naz@naz-HP-xw6600-Workstation>
2013-06-14 11:01:36 +0200

    Initilizing packages

---
+++ src/main/java/org/geosdi/geoservermq/DummyClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.geosdi.geoservermq;
+
+/**
+ * @author Nazzareno Sileno - CNR IMAA geoSDI Group
+ * @email nazzareno.sileno@geosdi.org
+ */
+public class DummyClass {
+
+}
